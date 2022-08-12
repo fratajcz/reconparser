f87b6565b32ac9a222ee6b26408c1d8566a211d5
INET Florin Ratajczak <florin.ratajczak@hpc-submit01.scidom.de>
2022-08-12 11:10:56 +0200

    started implementing the weighted option

--- parse_recon.py
+++ parse_recon.py
@@ -16,8 +16,8 @@ parser.add_argument('--filtered', "-f", type=str, default="data/filtered_metabol
 parser.add_argument('--ignore-components', "-c", action='store_true',
                     help="Wheter component codes ([c], [l], etc.) should be ignored.")
 parser.add_argument('--use_direction', "-d", action='store_true',
-                    help="Adds an edge from A to B only if A produces what B consumes. NOT IMPLEMENTED")
-parser.add_argument('--use_weight', "-w", action='store_true',
+                    help="Adds an edge from A to B only if A produces what B consumes.")
+parser.add_argument('--use_weights', "-w", action='store_true',
                     help="Adds an additional column 'Weight' which displays stochiometry. NOT IMPLEMENTED")
 
 args = parser.parse_args()
@@ -25,12 +25,11 @@ args = parser.parse_args()
 
 def generate_edges(producing_geneset, consuming_geneset=None):
     """ generates edges from genesets. 
-    
+
     If only producing geneset is provided, it is assumed that we are in undirected setting and all possible combinations are produces
     If both sets are provided, only edges running from all producing to all consuming genes are provided.
 
-    RETURNS list of tuples
-    
+    RETURNS list of lists.
     """
 
     if consuming_geneset is None:
@@ -39,6 +38,11 @@ def generate_edges(producing_geneset, consuming_geneset=None):
         edges = [[producer, consumer] for consumer in consuming_geneset for producer in producing_geneset]
     return edges
 
+if args.use_weights:
+    raise NotImplementedError("Weights are not yet implemented due to ambiguity of their meaning.")
+
+edge_template = "{}\t{}\t{}\t{}\n" if args.use_weights else "{}\t{}\t{}\n"
+file_header = ["EntrezA", "EntrezB", "Metabolite", "Weight"] if args.use_weights else ["EntrezA", "EntrezB", "Metabolite"]
 
 mat = scipy.io.loadmat(args.input)
 recon = mat["Recon3D"][0][0]
@@ -67,6 +71,7 @@ metabolites2reactions = recon[0][mask, :]
 filtered_metabolites = metabolites[~mask]
 metabolites = metabolites[mask]
 reactions2genes = recon[21]
+entrez_ids = recon[9]
 
 print("filtered out {} from a total of {} metabolites. See {} for a list of them.".format(np.sum(~mask), len(mask), filtered_list))
 
@@ -77,24 +82,23 @@ with open(filtered_list, "w") as file:
 genes2metabolites = (metabolites2reactions * reactions2genes).transpose()
 
 if not args.use_direction:
-    genes2metabolites[genes2metabolites != 0] = 1
+    if not args.use_weights:
+        genes2metabolites[genes2metabolites != 0] = 1
     genes2metabolites = genes2metabolites.astype(np.bool8)
 else:
     producing_genes2metabolites = genes2metabolites.copy().astype(np.bool8)
     consuming_genes2metabolites = genes2metabolites.copy().astype(np.bool8)
 
-    producing_genes2metabolites[genes2metabolites > 0] = 1
+    if not args.use_weights:
+        producing_genes2metabolites[genes2metabolites > 0] = 1
     producing_genes2metabolites[genes2metabolites <= 0] = 0
 
-    consuming_genes2metabolites[genes2metabolites < 0] = 1
+    if not args.use_weights:
+        consuming_genes2metabolites[genes2metabolites < 0] = 1
     consuming_genes2metabolites[genes2metabolites >= 0] = 0
 
-
-
-entrez_ids = recon[9]
-
 with open(output_file, "w") as edgelist:
-    edgelist.write("{}\t{}\t{}\n".format("EntrezA", "EntrezB", "Metabolite"))
+    edgelist.write(edge_template.format(*file_header))
 
 written_edges = 0
 
@@ -129,7 +133,7 @@ for i, col in enumerate(range(genes2metabolites.shape[1])):
                 if args.ignore_components:
                     all_edges.add(edge_tuple)
                 else:
-                    edgelist.write("{}\t{}\t{}\n".format(*edge_tuple))
+                    edgelist.write(edge_template.format(*edge_tuple))
 
     if i % 500 == 0 or i == genes2metabolites.shape[1] - 1:
         verb = "Gathered" if args.ignore_components else "Wrote"
@@ -141,7 +145,7 @@ if args.ignore_components:
         all_edges.sort(key=lambda x: x[2])
 
         for i, edge in enumerate(all_edges):
-            edgelist.write("{}\t{}\t{}\n".format(*edge))
+            edgelist.write(edge_template.format(*edge))
 
             if i % 100000 == 0 or i == len(all_edges) - 1:
                 print("Written {} out of {} deduplicated Edges.".format(i, len(all_edges)))
